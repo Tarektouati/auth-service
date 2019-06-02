822ac86843f125dd460dc34304f68444ea77a1ed
Tarektouati <209480@supinfo.com>
2019-06-02 12:28:43 +0200

    feat/added Linter (hlint) + code prettifier (stylish-haskell)

--- src/Handler.hs
+++ src/Handler.hs
@@ -1,30 +1,26 @@
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE DataKinds, DuplicateRecordFields, OverloadedStrings, TypeOperators #-}
 
 
-module Handler 
+module Handler
           ( verify
           , generate
           ) where
 
-import Token (decodeToken, createToken, verifyToken)
-import Types (Generate(..),Verify(..),Response(..))
-import Servant (Handler)
-import Data.Text (Text)
+import           Data.Text (Text)
+import           Servant (Handler)
+import           Token (createToken, decodeToken, verifyToken)
+import           Types (Generate (..), Response (..), Verify (..))
 
 
 verify :: Text -> Verify -> Handler Response
-verify secret Verify{jwt=t} = do
-  case (verifyToken secret $ t) of
+verify secret Verify{jwt=t} =
+  case  verifyToken secret t of
     Nothing -> return $ Response "Token not verified" False Nothing Nothing
-    Just x -> return $ 
+    Just x -> return $
                 Response "Token verified" True (Just t) (Just user)
                 where user = decodeToken x
-       
+
 generate :: Text -> Generate -> Handler Response
-generate secret Generate{user = u} = do
-  let token = createToken secret $ u
-    in return $ Response "Token generated succefully" True (Just token) Nothing
+generate secret Generate{user = u} =
+  return $ Response "Token generated succefully" True (Just token) Nothing
+  where token = createToken secret u
